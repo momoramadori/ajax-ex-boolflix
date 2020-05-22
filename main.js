a0ff598814bd3ba80a28d3fefaebbdba836cbdc9
Momo Ramadori <momoramadori8@gmail.com>
2020-05-22 17:11:53 +0200

    aggiunti due bonus per ottimizzare la ricerca

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
     var source   = $("#entry-template").html();
     var template = Handlebars.compile(source);
 
+    //intercetto il click sul bottone
     $('button').click(function() {
+        //nascondo tutti i film
+        $('div.entry').hide();
+        //nascondo l'avviso di nessun risultato
+        $('.no-results').removeClass('active')
+        //creo una variabile per salvare l'input dell'utente
         var testo_utente = $('input').val();
         $.ajax({
             'url':'https://api.themoviedb.org/3/search/movie',
@@ -12,27 +18,50 @@ $(document).ready(function() {
                 'query': testo_utente
             },
             'success': function(data) {
-                for (var i = 0; i < data.results.length; i++) {
-                    var risultati = data.results[i];
-                    var titolo = risultati.title;
-                    var titolo_originale = risultati.original_title;
-                    var lingua = risultati.original_language;
-                    var voto_medio = risultati.vote_average;
-                    var img = risultati.poster_path;
-                    var context = {
-                        'title': titolo,
-                        'original_title': titolo_originale,
-                        'language': lingua,
-                        'vote': voto_medio,
+                var founded_films = data.results;
+                genera_dati(founded_films);
+
+                //scorro i singoli risultati ottenuti e
+                $('.entry').each(function(){
+                    // se il titolo è uguale al titolo originale li nascondo
+                    if($(this).find('h4').attr('value') == $(this).find('p:first-of-type').attr('value')) {
+                        $(this).find('p:first-of-type').remove();
                     }
-                    var html = template(context);
-                    $('main').append(html)
+                })
+                //se l'oggetto di array dei risultati è vuoto rendo visibile l'avviso di nessun risultato
+                if(data.results.length == 0) {
+                    $('.no-results').addClass('active');
                 }
             },
             'error': function(){
                 alert('error!')
             }
-
         })
     })
+
+    function disegna_film(risultati) {
+        //salvo i valori delle chiavi che mi interessano
+        var titolo = risultati.title;
+        var titolo_originale = risultati.original_title;
+        var lingua = risultati.original_language;
+        var voto_medio = risultati.vote_average;
+        //creo un oggetto da poter inserire nel template di handlebars con i corrispondenti valori
+        var context = {
+            'title': titolo,
+            'original_title': titolo_originale,
+            'language': lingua,
+            'vote': voto_medio,
+        }
+        var html = template(context);
+        $('main').append(html)
+    }
+
+    function genera_dati(film_trovati) {
+        //utilizzo un ciclo per scorrere gli oggetti e reperire ciò che mi interessa
+        for (var i = 0; i < film_trovati.length; i++) {
+            //utilizzo una variabile per salvare gli oggetti
+            var risultati = film_trovati[i];
+            disegna_film(risultati)
+        }
+    }
 })
