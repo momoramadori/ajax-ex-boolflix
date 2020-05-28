721243ddec5b9a8342b1c1cf47ce592fc9476796
Momo Ramadori <momoramadori8@gmail.com>
2020-05-28 16:45:28 +0200

    Milestone 5 ended

--- main.js
+++ main.js
@@ -12,12 +12,16 @@ $(document).ready(function() {
         // verifico se l'utente ha digitato "ENTER"
         if(event.which == 13) {
             all_search();
+
         }
-    });
+    })
 
     //funzione per le la ricerca utente
     function all_search() {
-        $('input').addClass('active');
+        //rendo visibili i filtri
+        $('select').addClass('active');
+        $('label').addClass('active');
+        
         //creo una variabile per salvare l'input dell'utente
         var testo_utente = $('input').val();
         //se l'input non ha caratteri non faccio la chiamata ajax
@@ -106,59 +110,14 @@ $(document).ready(function() {
             'immagine': genera_immagini(risultati.poster_path),
             'id': risultati.id,
             'riassunto': riassunto(risultati.overview),
-            'tipo': tipologia
+            'tipo': tipologia,
+            'stringa_generi': risultati.genre_ids.join(',')
         }
         var html = template(context);
         $('main').append(html);
-        genera_cast(risultati.id,risultati)
+        genera_cast(risultati.id,risultati);
+        genera_genres(risultati)
     }
-
-    
-    //funzione per recuperare  il cast dei film
-    function genera_cast(id,risultati) {
-        if ( risultati.hasOwnProperty('original_title')) {
-            var url = 'https://api.themoviedb.org/3/movie/' + id + '/credits'
-        } else {
-            var url = 'https://api.themoviedb.org/3/tv/' + id + '/credits'
-        }
-        $.ajax({
-            'url': url,
-            'method':'GET',
-            'data':{
-                'api_key': api_key,
-            },
-            'success': function(data) {
-                var pippo = genera_credits(data);
-                if (pippo != '') {
-                    $('.entry[data-id='+id+'] p.cast').append(pippo);
-                } else {
-                    $('.entry[data-id='+id+'] p.cast').append('no cast availale!');
-                }
-            },
-            'error': function() {
-                alert('error!')
-            }
-        }) 
-    }
-        
-    //funzione che restitutisce i credits dei singoli film/serie
-    function genera_credits(data) {
-        var array = [];
-        var index = 0;
-            do {
-                if(data.cast[index] != undefined && data.cast[index].name != undefined) {
-                    if (data.cast[index].hasOwnProperty('name')) {
-                        var cast = data.cast[index].name;
-                        array.push(cast)
-                        index++
-                    } 
-                }
-            } while (array.length < 5 && array.length < data.cast.length);
-            var cast = array.toString();
-            return cast
-    }
-
-    
 
     //funzione per generare le stelle
     function genera_stelle(voti) {
@@ -202,7 +161,148 @@ $(document).ready(function() {
             contenuto = 'no overview available!'
         } return contenuto
     }
+
+    //funzione per recuperare  il cast dei film
+    function genera_cast(id,risultati) {
+        if ( risultati.hasOwnProperty('original_title')) {
+            var url = 'https://api.themoviedb.org/3/movie/' + id + '/credits'
+        } else {
+            var url = 'https://api.themoviedb.org/3/tv/' + id + '/credits'
+        }
+        $.ajax({
+            'url': url,
+            'method':'GET',
+            'data':{
+                'api_key': api_key,
+            },
+            'success': function(data) {
+                var pippo = genera_credits(data);
+                if (pippo != '') {
+                    $('.entry[data-id='+id+'] p.cast').append(pippo);
+                } else {
+                    $('.entry[data-id='+id+'] p.cast').append('no cast availale!');
+                }
+            },
+            'error': function() {
+                alert('error!')
+            }
+        }) 
+    }
+
+    //funzione che permette di ottenere con chiamata ajax le liste dei generi di film e serie tv
+    function genera_genres(risultati) {
+        if ( risultati.hasOwnProperty('original_title')) {
+            var url = 'https://api.themoviedb.org/3/genre/movie/list'
+        } else {
+            var url = 'https://api.themoviedb.org/3/genre/tv/list'
+        }
+        $.ajax({
+            'url': url,
+            'method':'GET',
+            'data':{
+                'api_key': api_key,
+            },
+            'success': function(data) {
+                genera_lista_generi(data,risultati)
+            },
+            'error': function() {
+                alert('error!')
+            }
+        }) 
+    }
+
+     //funzione che restitutisce i credits dei singoli film/serie
+     function genera_credits(data) {
+        var array = [];
+        var index = 0;
+        do {
+            if(data.cast[index] != undefined && data.cast[index].name != undefined) {
+                if (data.cast[index].hasOwnProperty('name')) {
+                    var cast = data.cast[index].name;
+                    array.push(cast)
+                    index++
+                } 
+            }
+        } while (array.length < 5 && array.length < data.cast.length);
+        var cast = array.toString();
+        return cast
+    }
+
+    //funzione per ottenere la lista
+    function genera_lista_generi(data,risultati) {
+        var lista_generi_totali = [];
+        var lista_generi_risultato = [];
+        //creo array con tutti i numeri dei generi
+        for (let index = 0; index < data.genres.length; index++) {
+            var id_genere = data.genres[index].id
+            lista_generi_totali.push(id_genere)  
+        }
+        
+        //ciclo tutti gli id dei generi del risultato
+        for (let index = 0; index < risultati.genre_ids.length; index++) {
+            for (let i = 0; i < data.genres.length; i++) {
+                if( risultati.genre_ids[index] == data.genres[i].id) {
+                    var genere = data.genres[i].name
+                    lista_generi_risultato.push(genere) 
+                }  
+            }  
+        }
+        var numeri_generi = risultati.genre_ids.join(',');
+        var lista_stringa = lista_generi_risultato.join(', ')
+        if(numeri_generi != '') {
+            $('.entry[data-id="'+risultati.id+'"] p.generi').append(lista_stringa)
+        }
+}
+
+function select_film() {
+    $.ajax({
+        'url': 'https://api.themoviedb.org/3/genre/movie/list',
+        'method':'GET',
+        'data':{
+            'api_key': api_key,
+        },
+        'success': function(data) {
+            for (let index = 0; index < data.genres.length; index++) {
+                var nome_genere = data.genres[index].name;
+                $('#select').append('<option value="'+nome_genere+'">'+nome_genere+'</option>')
+            }
+            $('label[for="select"]').text('Filtra Film per genere')
+        },
+        'error': function() {
+            alert('error!')
+        }
+    }) 
+}
+
+select_film()
+select_tv()
+
+function select_tv() {
+    $.ajax({
+        'url': 'https://api.themoviedb.org/3/genre/tv/list',
+        'method':'GET',
+        'data':{
+            'api_key': api_key,
+        },
+        'success': function(data) {
+            array_generi = [];
+            for (let index = 0; index < data.genres.length; index++) {
+                var nome_genere = data.genres[index].name;
+                $('#select-2').append('<option value="'+nome_genere+'">'+nome_genere+'</option>')
+            }
+            $('label[for="select-2"]').text('Filtra Serie Tv per genere')
+            console.log(array_generi);
+            
+        },
+        'error': function() {
+            alert('error!')
+        }
+    }) 
+}
 })
+
+
+
 
 
 
